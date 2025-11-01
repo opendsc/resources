d5a5c2421ee79b17b2739c101aad5951916c0615
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-10-31 21:04:12 -0500

    Add windows-environment-variable

---
+++ windows-environment-variable/src/Schema.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Thomas Nieto - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the MIT license.
+
+using System.Text.Json.Serialization;
+
+using Json.Schema.Generation;
+
+namespace OpenDsc.Resource.Windows.EnvironmentVariable;
+
+[Title("Windows Environment Variable Schema")]
+[Description("Schema for managing Windows environment variables via OpenDsc.")]
+public sealed class Schema
+{
+    [Required]
+    [Description("The name of the environment variable.")]
+    [Pattern(@"^[^\x00=][^=]{0,253}$")]
+    public string Name { get; set; } = string.Empty;
+
+    [Description("The environment variable value.")]
+    [Nullable(false)]
+    public string? Value { get; set; }
+
+    [JsonPropertyName("_exist")]
+    [Description("Indicates whether the environment variable exists.")]
+    [Nullable(false)]
+    [Default(true)]
+    public bool? Exist { get; set; }
+
+    [JsonPropertyName("_scope")]
+    [Description("If environment variable should be user or machine scope.")]
+    [Nullable(false)]
+    [Default("User")]
+    public Scope? Scope { get; set; }
+}
